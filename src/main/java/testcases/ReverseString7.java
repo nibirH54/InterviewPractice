f44734ffec1161ae7c6096782bce94b7eea84a04
nibirH54 <nibirw@gmail.com>
2023-01-15 20:46:05 -0500

    New Commit of interview Questions.

---
+++ src/main/java/testcases/ReverseString7.java
@@ -0,0 +1,15 @@
+package testcases;
+
+public class ReverseString7 {
+    public static void main(String[] args) {
+
+        String s = "Automation";//noitamotua
+        int len = s.length();
+        String rev = "";
+
+        for(int i = len -1; i >= 0; i--){
+            rev = rev + s.charAt(i);
+        }
+        System.out.println(rev);
+    }
+}
